85cb1c62e94ee8102e291ddd92a92f8f49cd2758
NehemiahLimo <nehemiahlimo02@gmail.com>
2023-01-04 02:36:38 +0300

    Initial commit

---
+++ Pages/Account/Login.cshtml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using static webapp_security.Pages.IndexModel;
+
+namespace webapp_security.Pages.Account
+{
+	public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public Credential Credentials { get; set; }
+        public void OnGet()
+        {
+            //this.Credentials = new Credential { UserName = "NehemiahLimo" };
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+            
+                if(Credentials.UserName=="admin" && Credentials.Password == "admin")
+                {
+                    var claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, "admin"),
+                        new Claim(ClaimTypes.Email, "admin@codesofff.ccom")
+
+                    };
+                    var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+                    var principal = new ClaimsPrincipal(identity);
+
+                    await HttpContext.SignInAsync("MyCookieAuth", principal);
+
+                    return RedirectToPage("/Index");
+                }
+            //create context
+            return Page();
+
+
+        }
+
+        public class Credential
+        {
+            [Required]
+            [Display(Name ="User Name")]
+            public string UserName { get; set; }
+            [Required]
+            [Display(Name = "Password")]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+        }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webapp_security.Pages;
+
+public class IndexModel : PageModel
+{
+    
+    private readonly ILogger<IndexModel> _logger;
+
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+
+    
+}
+
